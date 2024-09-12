daa816e471cab9ecab182449836298a6b064ed46
Diaran Cruz <diaran.real@gmail.com>
2024-09-12 15:08:09 -0300

    commit1

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// variavel guarda
+// função e para fazer
+
+
+// function mostrarNome() {
+//     let nomeUsuario = document.getElementById("nomeUsuario").value
+//     document.getElementById("paragrafo").innerText = nomeUsuario
+// }
+
+function mostrarNome() {
+    let nomeUsuario = document.getElementById("nomeUsuario").value
+    document.getElementById("paragrafo").innerText = "Bem vindo, " + nomeUsuario
+}
+
+function calcAreaCirculo() {
+    let raio = document.getElementById("raio").value
+    const pi = 3.14
+    let area = pi * raio**2
+    document.getElementById("area").innerText = area
+}
+
+function soma() {
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)    
+    let soma = n1 + n2
+    document.getElementById("soma").innerText = soma
+}
